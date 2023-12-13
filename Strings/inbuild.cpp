2400ec0103fdca755acf2d0a20275c19e8ff6d05
tarun028 <tarunsrivastavakvs@gmail.com>
2023-12-13 11:55:08 +0530

    day4

---
+++ Strings/inbuild.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char str[100];
+    cin>>str;
+
+    int len = strlen(str);
+    cout<<len<<endl;
+    return 0;
+}
